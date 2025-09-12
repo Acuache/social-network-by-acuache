eadf65c53f4f6c93bb46a2cdc5be4af010ed1e5c
AcuacheY <v.acuache15@gmail.com>
2025-09-11 21:09:09 -0500

    feat(PublicationImage): add componente

--- src/components/Home/CardPublication.tsx
+++ src/components/Home/CardPublication.tsx
@@ -1,9 +1,10 @@
-import { PublicationHeader, PublicationComment } from './'
+import { PublicationHeader, PublicationComment, PublicationImage } from './'
 export default function CardPublication() {
   return (
     <article className="border-y-2  border-gray-500/50 p-4">
       <PublicationHeader />
       <PublicationComment />
+      <PublicationImage />
     </article>
   )
 }
--- src/components/Home/PublicationImage.tsx
+++ src/components/Home/PublicationImage.tsx
@@ -1,7 +1,7 @@
 export default function PublicationImage() {
   return (
-    <div>
-
+    <div className="rounded-lg overflow-hidden bg-red-900 flex justify-center items-center">
+      <img className="w-max-[500px] max-h-[500px] object-contain" src="https://m.media-amazon.com/images/I/717I9g5Nf9L._AC_SL1000_.jpg" alt="" />
     </div>
   )
 }
